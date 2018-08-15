c6053ab9fd56fdd8413ab3334c8b899953e738c0
Kia Panahi Rad <kia.panahirad@gmail.com; kia@appson.ir>
2018-08-15 16:48:14 +0430

    Added sample message scheme

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from mattermostdriver import Driver
 from tools import utils, driver
 import asyncio
+import json
 
 
 def main():
@@ -23,7 +24,45 @@ def main():
 
 
 @asyncio.coroutine
-def sample_handler(message):
+def sample_handler(message_string):
+    """
+    {
+        "event": "posted",
+        "data": {
+            "channel_display_name":"",
+            "channel_name":"a4tkpy6ifidc8pwz9pnob69yfr__oudjdi8mij8d3cuc8czumkrkpa",
+            "channel_type":"D",
+            "mentions":"[\"oudjdi8mij8d3cuc8czumkrkpa\"]",
+            "post":"{
+                    \"id\":\"kdxkhsyidb8zm8wt1nidsktqeh\",
+                    \"create_at\":1534334279971,
+                    \"update_at\":1534334279971,
+                    \"edit_at\":0,
+                    \"delete_at\":0,
+                    \"is_pinned\":false,
+                    \"user_id\":\"a4tkpy6ifidc8pwz9pnob69yfr\",
+                    \"channel_id\":\"fxs17e67hprgmx35xabo1w98qr\",
+                    \"root_id\":\"\",
+                    \"parent_id\":\"\",
+                    \"original_id\":\"\",
+                    \"message\":\"message!\",
+                    \"type\":\"\",
+                    \"props\":{},
+                    \"hashtags\":\"\",
+                    \"pending_post_id\":\"a4tkpy6ifidc8pwz9pnob69yfr:1534334297887\"
+                   }",
+            "sender_name":"shadi",
+            "team_id":""
+        },
+        "broadcast": {
+            "omit_users":null,
+            "user_id":"",
+            "channel_id":"fxs17e67hprgmx35xabo1w98qr",
+            "team_id":""
+        },
+        "seq": 2
+    }
+    """
     print(message)
 
 
