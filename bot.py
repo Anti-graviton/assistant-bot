62f8bd7041312aa7f862147bdc830b5fffda323d
Kia Panahi Rad <kia.panahirad@gmail.com; kia@appson.ir>
2018-08-16 11:04:28 +0430

    fixed infitine loop message processing

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ from tools import utils, driver
 import asyncio
 import json
 
+PLATE_NUMBER_PATTERN = r'(ایران|ايران)[\s]*[\d]{2} [\d]{2}[\w]{1}[\d]{3}'
+
 
 def main():
 
@@ -25,31 +27,52 @@ def main():
 
 @asyncio.coroutine
 def sample_handler(message_string):
+
     message = json.loads(message_string)
 
+    bot = driver.users.get_user('me')
+
     if 'event' not in message:
-        # print('passing cuz event was not in message')
         return
 
     if message['event'] != 'posted':
-        # print('passing cuz event was not "posted"')
         return
 
     if 'data' not in message:
         return
+
+    team = driver.teams.get_team_by_name('ITTest')
+    channel = driver.channels.get_channel_by_name(
+        team['id'], message['data']['channel_name'])
 
     if message['data']['channel_type'] != 'D':
         return
 
     post = json.loads(message['data']['post'])
 
+    if post['user_id'] == bot['id']:
+        return
+
     message_content = post['message'].lower()
 
     if message_content == 'register':
-        print('getting registered!')
+        user = driver.users.get_user(post['user_id'])
+        register(channel, user)
         return
 
-    print('message did not match any criteria')
+    send_message(channel, 'sorry, I did not understand your command')
+
+
+def register(channel, user):
+    send_message(channel, 'we will do something here later')
+    pass
+
+
+def send_message(channel, message):
+    driver.posts.create_post(options={
+        'channel_id': channel['id'],
+        'message': message
+    })
 
 
 if __name__ == '__main__':
