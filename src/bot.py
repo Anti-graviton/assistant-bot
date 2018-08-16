c94a82d2115cafebe3b04b138adb8557a159aba3
Hossein Taleghani <sh.taleghani@gmail.com>
2018-08-16 13:08:10 +0430

    Set logger and settings module

--- src/bot.py
+++ src/bot.py
@@ -1,5 +1,17 @@
-from mmpy_bot.bot import Bot, MattermostClientv4, PluginsManager, MessageDispatcher
+# -*- coding: utf-8 -*-
+import sys
+import os
+import logging
 from settings import settings
+os.environ['MATTERMOST_BOT_SETTINGS_MODULE'] = 'settings.settings'
+from mmpy_bot.bot import Bot, MattermostClientv4, PluginsManager, MessageDispatcher
+
+logging.basicConfig(**{
+    'format': '[%(asctime)s] %(message)s',
+    'datefmt': '%m/%d/%Y %H:%M:%S',
+    'level': logging.DEBUG if settings.DEBUG else logging.INFO,
+    'stream': sys.stdout,
+})
 
 class AssistantBot(Bot):
     def __init__(self):
@@ -12,5 +24,5 @@ class AssistantBot(Bot):
         self._plugins.init_plugins()
         self._dispatcher = MessageDispatcher(self._client, self._plugins)
 
-if __name__ == "__main__":    
+if __name__ == "__main__":
     AssistantBot().run()
