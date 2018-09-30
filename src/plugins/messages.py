1ab66c4cd0b2c60f9c5be1b58459e3f1b4af1e38
Hossein Taleghani <sh.taleghani@gmail.com>
2018-09-30 17:14:09 +0330

    Change 'lottery' to 'event' in messages

--- src/plugins/messages.py
+++ src/plugins/messages.py
@@ -2,11 +2,11 @@
 
 
 class Messages(object):
-    NOT_VALID_EVENT = "در حال حاضر قرعه کشی فعالی وجود ندارد"
+    NOT_VALID_EVENT = "در حال حاضر رویداد فعالی وجود ندارد"
     DEFINE_CAR = "لطفا یک ماشین تعریف کنید"
     ALREADY_REGISTERED = "خیالت راحت، ثبت‌نام کردی"
     SUCCESSFUL_REGISTERATION = ":+1:"
-    UNDO_REGISTERATION = "انصراف از قرعه‌کشی ثبت شد"
+    UNDO_REGISTERATION = "انصراف از رویداد ثبت شد"
     NOT_YET_REGISTERED = "گرفتی ما رو؟! اصلا ثبت‌نام نکردی که"
     NO_CAR_EXISTS = "پوچ!"
     CAR_REMOVED = "اطلاعات ماشین حذف شد"
@@ -15,7 +15,7 @@ class Messages(object):
     CAR_REGISTERED = "اطلاعات ماشین ثبت شد"
     CAR_AND_PARTICIPATION_REGISTERED = "اطلاعات ماشین ثبت شد و شما در"\
         " قرعه‌کشی شرکت داده شدید. برای انصراف بزنید `unreg`"
-    NEW_EVENT_REGISTERED = "قرعه کشی جدید ثبت گردید"
-    EVENT_ALREADY_EXISTS = "امکان ثبت قرعه‌کشی دیگری بصورت هم‌زمان وجود ندارد"
-    EVENT_DEACTIVED = "قرعه کشی با موفقیت غیرفعال شد"
-    NO_EVENT_EXISTS = "هیچ قرعه‌کشی‌ای یافت نشد"
+    NEW_EVENT_REGISTERED = "رویداد جدید ثبت گردید"
+    EVENT_ALREADY_EXISTS = "امکان ثبت رویداد دیگری بصورت هم‌زمان وجود ندارد"
+    EVENT_DEACTIVED = "رویداد با موفقیت غیرفعال شد"
+    NO_EVENT_EXISTS = "هیچ رویدادی یافت نشد"
